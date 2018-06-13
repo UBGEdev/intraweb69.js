d7332d7a8d03bc007970fe7928c6bde16158899b
UBGEdev <36565196+UBGEdev@users.noreply.github.com>
2018-06-13 13:12:51 -0300

    Update script1.js
    Green Type

--- script1.js
+++ script1.js
@@ -1,4 +1,9 @@
 window.onload = function(){alert("Some Xss meme editing");
 document.getElementById("MainContent_TipoUsuario").textContent = "Teste";
 document.getElementsByClassName("logo-lg")[0].textContent =" Intraweb 6.9";
+document.getElementsByClassName("navbar navbar-static-top")[0].style.backgroundColor = "#11af3b";
+document.getElementsByClassName("logo")[0].style.backgroundColor = "#11af3b";
+document.getElementsByClassName("logo-lg")[0].style.backgroundColor = "#11af3b";
+document.getElementsByClassName("sidebar-toggle")[0].style.backgroundColor = "#11af3b";
+                          
 }
