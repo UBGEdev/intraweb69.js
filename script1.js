501ff58f52262f9a8804b400fa5efcb05535016a
UBGEdev <36565196+UBGEdev@users.noreply.github.com>
2018-06-13 12:48:04 -0300

    Create script1.js

---
+++ script1.js
@@ -0,0 +1,4 @@
+window.onload = function(){alert("Some Xss meme editing");
+document.getElementById("MainContent_TipoUsuario").textContent = "Teste";
+document.getElementsByClassName("logo-lg")[0].textContent =" Intraweb 6.9";
+}
