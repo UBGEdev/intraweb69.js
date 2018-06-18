f62a6009d0e1e4c7b608a8d505ac072f8ce40603
UBGEdev <36565196+UBGEdev@users.noreply.github.com>
2018-06-18 13:43:33 -0300

    Rozinha bem gay
    Atualizando sapoha pra ficar mais gay q o site

--- script1.js
+++ script1.js
@@ -1,9 +1,10 @@
 window.onload = function(){alert("Some Xss meme editing");
+var color = "ff00bb";
 document.getElementById("MainContent_TipoUsuario").textContent = "Teste";
 document.getElementsByClassName("logo-lg")[0].textContent =" Intraweb 6.9";
-document.getElementsByClassName("navbar navbar-static-top")[0].style.backgroundColor = "#11af3b";
-document.getElementsByClassName("logo")[0].style.backgroundColor = "#11af3b";
-document.getElementsByClassName("logo-lg")[0].style.backgroundColor = "#11af3b";
-document.getElementsByClassName("sidebar-toggle")[0].style.backgroundColor = "#11af3b";
+document.getElementsByClassName("navbar navbar-static-top")[0].style.backgroundColor = color;
+document.getElementsByClassName("logo")[0].style.backgroundColor = color;
+document.getElementsByClassName("logo-lg")[0].style.backgroundColor = color;
+document.getElementsByClassName("sidebar-toggle")[0].style.backgroundColor = color;
                           
 }
