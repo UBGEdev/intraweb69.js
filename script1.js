4767c8c927159dc0e4420062c866232184d39d3e
UBGEdev <36565196+UBGEdev@users.noreply.github.com>
2018-06-18 13:47:16 -0300

    sapoha funfa agr

--- script1.js
+++ script1.js
@@ -1,10 +1,10 @@
 window.onload = function(){alert("Some Xss meme editing");
-var color = "ff00bb";
+var color = "#ff00bb";
 document.getElementById("MainContent_TipoUsuario").textContent = "Teste";
-document.getElementsByClassName("logo-lg")[0].textContent =" Intraweb 6.9";
-document.getElementsByClassName("navbar navbar-static-top")[0].style.backgroundColor = color;
-document.getElementsByClassName("logo")[0].style.backgroundColor = color;
-document.getElementsByClassName("logo-lg")[0].style.backgroundColor = color;
-document.getElementsByClassName("sidebar-toggle")[0].style.backgroundColor = color;
+document.getElementsByClassName("logo-lg")[0].textContent =" TeraJhonsons 6.9";
+document.getElementsByClassName("navbar navbar-static-top")[0].style.backgroundColor = "#ff00bb";
+document.getElementsByClassName("logo")[0].style.backgroundColor = "#ff00bb";
+document.getElementsByClassName("logo-lg")[0].style.backgroundColor = "#ff00bb";
+document.getElementsByClassName("sidebar-toggle")[0].style.backgroundColor = "#ff00bb";
                           
 }
